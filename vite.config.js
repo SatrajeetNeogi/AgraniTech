dcf532e34cd0baf5d50014065e3bf74fe936784a
SatrajeetNeogi <145013589+SatrajeetNeogi@users.noreply.github.com>
2024-08-21 12:52:06 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: "build", // Your custom output directory
+  },
+  base: '/AgraniTech/',  // Set this to your repository name for GitHub Pages
+});
